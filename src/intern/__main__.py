dfc2fb165f6fbdbfbcbdc884e1e863bff7c4a550
thuhien191197 <thuhien191197@gmail.com>
2019-05-06 17:53:57 +0700

    update README.me

--- src/intern/__main__.py
+++ src/intern/__main__.py
@@ -1,3 +1,13 @@
 from .core import app
+from .endpoint.simple_view import SimpleView
+from .endpoint.greeting import Greeting
+from .endpoint.goodbye import Goodbye
+
+
+app.add_route(SimpleView.as_view(), '/simple')
+
+app.add_route(Greeting.as_view(), '/')
+
+app.add_route(Goodbye.as_view(), '/goodbye')
 
 app.run(host='0.0.0.0', port=8888)
